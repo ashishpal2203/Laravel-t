fbc0c6b48ef99b1b1c25185e14b4efc0a474f04e
Ashish Pal <min@codecrafter.biz>
2024-01-31 18:49:52 +0530

    first commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,7 @@
    <div class="container">
     <div class="row">
         <div class="col-lg-6">
+            <h3>All user's list</h3>
             <table class="table">
                 <thead>
                   <tr>
@@ -37,6 +38,20 @@
    </div>
 </div>
 
+@php
+    $ageData = $users->map(function ($user) {
+        $user->age = \Carbon\Carbon::parse($user->age)->age;
+    })->groupBy(function ($user) {
+        if ($user->age < 18) {
+            return 'Below 18';
+        } elseif ($user->age <= 40) {
+            return '18 to 40';
+        } else {
+            return 'Above 40';
+        }
+    })->map->count(); 
+@endphp
+
 <script type="text/javascript">
     google.charts.load('current', {
         'packages': ['corechart']
@@ -46,17 +61,16 @@
     function drawChart() {
 
         var data = google.visualization.arrayToDataTable([
-            ['Task', 'Hours per Day'],
-            ['DOCTOR', 5],
-            ['PATIENT', 7],
-            ['LAB', 50],
-            ['PHARMA', 178],
+            ['Age Group', 'Number of Users'],
+            ['Below 18', {{ $ageData['Below 18'] ?? 0 }}],
+            ['18 to 40', {{ $ageData['18 to 40'] ?? 0 }}],
+            ['Above 40', {{ $ageData['Above 40'] ?? 0 }}]
         ]);
 
         var options = {
             title: '',
             is3D: true,
-            //   pieHole: 0.4,
+
 
         };
 
